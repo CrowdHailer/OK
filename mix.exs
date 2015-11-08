3c8041b461133d584ac710c2c55980383a96f4d3
CrowdHailer <peterhsaxton@gmail.com>
2015-11-08 19:41:27 +0000

    version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule OK.Mixfile do
 
   def project do
     [app: :ok,
-     version: "0.1.2",
+     version: "0.1.3",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
