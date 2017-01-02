28f283e4d4cb25336cb508e40fcf18b89a85a7e7
CrowdHailer <peterhsaxton@gmail.com>
2017-01-02 11:46:23 +0000

    new release

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule OK.Mixfile do
 
   def project do
     [app: :ok,
-     version: "1.1.0",
+     version: "1.2.0",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
